48c1629a5a8c401b5cbed4565dbd7fb82453097a
udo_stenzel <udo_stenzel@93d9a472-daf7-0310-8f21-d7befa554562>
2011-04-26 14:21:15 +0000

    added buildall script
    git-svn-id: http://biofs04/svn/bioinf/biohazard/trunk@2708 93d9a472-daf7-0310-8f21-d7befa554562

---
+++ buildall.sh
@@ -0,0 +1,12 @@
+version=$( awk '/Version:/ { print $2 }' biohazard.cabal )
+echo "Building and installing biohazard-${version}"
+
+cabal clean
+cabal configure
+cabal build
+cabal install
+cabal sdist
+
+echo "SCP to bioinf..."
+scp dist/biohazard-${version}.tar.gz \
+    bioinf:htdocs/biohazard/
