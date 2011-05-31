3a22cff7f7e14295c8dd32d79196a98b2654ffab
udo_stenzel <udo_stenzel@93d9a472-daf7-0310-8f21-d7befa554562>
2011-05-31 11:36:51 +0000

    WIP
    git-svn-id: http://biofs04/svn/bioinf/biohazard/trunk@2760 93d9a472-daf7-0310-8f21-d7befa554562

--- Bio/File/Bgzf.hs
+++ Bio/File/Bgzf.hs
@@ -1,71 +1,83 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ForeignFunctionInterface, BangPatterns #-}
 module Bio.File.Bgzf where
 
-import qualified Data.ByteString as B
-import Data.ByteString ( ByteString )
-import qualified Data.Iteratee.ListLike as I
-import Data.Iteratee.Binary
-import Data.Bits
-import Control.Monad.IO.Class
 import Control.Monad
-import Foreign.Marshal.Alloc
-import Foreign.Marshal.Utils
-import Foreign.Storable
-import qualified Data.ByteString.Unsafe as B
-import Foreign.C.Types
-import Foreign.Ptr
-import Data.Word
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Data.Monoid
-import Data.Int
-import Data.Nullable
+-- import Foreign.Marshal.Alloc
+-- import Foreign.Marshal.Utils
+-- import Foreign.Storable
+-- import Foreign.C.Types
+-- import Foreign.Ptr
+import Data.Bits
+import Data.Int                             ( Int64 )
+import Data.Iteratee.Binary
+import Data.Iteratee.Char                   ( printLinesUnterminated )
 import Data.Iteratee.IO
-import Data.Iteratee.Char
+import Data.Monoid
+import Data.Word                            ( Word16 )
 import Codec.Compression.GZip
-import qualified Data.ByteString.Lazy as L
+
+import qualified Data.ByteString            as B
+import qualified Data.ByteString.Lazy       as L
+-- import qualified Data.ByteString.Unsafe     as B
+import qualified Data.Iteratee.ListLike     as I
+
 
 test = enumFile 16300 "/mnt/454/Altaiensis/bwa/catalog/EPO/combined_SNC_anno.tsv.bgz" $
-       decompressBgzf $ printLinesUnterminated
+       decompressBgzf printLinesUnterminated
 
-decompressBgzf :: MonadIO m => I.Iteratee ByteString m a -> I.Iteratee ByteString m a
-decompressBgzf inner = collectI get_bgzf_header $ \csize -> do
-                       comp <- get_block $ fromIntegral csize +1
-                       let c = B.concat . L.toChunks . decompress $ L.fromChunks [comp]
-                       liftIO $ print $ B.length c
+-- one BGZF block: offset and contents
+data Block = Block {-# UNPACK #-} !Int64 {-# UNPACK #-} !B.ByteString
 
+decompressBgzf' :: MonadIO m => (I.Iteratee Block m a) -> I.Iteratee B.ByteString m a
+decompressBgzf' = decompressBgzfWith Block 0
 
-                       -- c <- liftIO $ do pu <- mallocBytes (fromIntegral usize)
-                                        -- B.unsafeUseAsCStringLen comp $ \(pc, lc) -> do
-                                            -- guard (lc == fromIntegral csize + 1)
-                                            -- with (fromIntegral usize) $ \plu -> do
-                                                -- rc <- zlib_uncompress pu plu pc (fromIntegral lc)
-                                                -- unless (rc == 0) . fail $ "fuck, Zlib don't like me: " ++ show rc
-                                                -- print (plu, usize)
-                                                -- peek plu >>= guard . (fromIntegral usize ==)
-                                                -- B.packCStringLen (pu,fromIntegral usize)
-                       lift (I.enumPure1Chunk c inner) >>= decompressBgzf
+decompressBgzf :: MonadIO m => (I.Iteratee B.ByteString m a) -> I.Iteratee B.ByteString m a
+decompressBgzf = decompressBgzfWith (\_ s -> s) 0
+
+decompressBgzfWith :: MonadIO m => (Int64 -> B.ByteString -> s) -> Int64 -> (I.Iteratee s m a) -> I.Iteratee B.ByteString m a
+decompressBgzfWith block !off inner =
+    collectI get_bgzf_header $ \csize -> do
+    comp <- get_block $ fromIntegral csize +1
+    -- this is ugly and very roundabout, but works for the time being...
+    let c = B.concat . L.toChunks . decompress $ L.fromChunks [comp]
+    lift (I.enumPure1Chunk (block off c) inner) >>= decompressBgzfWith block (off + fromIntegral csize + 1)
+
+   -- Doesn't work.  Maybe because 'uncompress'
+   -- gets confused by the headers?
+   -- c <- liftIO $ do pu <- mallocBytes (fromIntegral usize)
+                    -- B.unsafeUseAsCStringLen comp $ \(pc, lc) -> do
+                        -- guard (lc == fromIntegral csize + 1)
+                        -- with (fromIntegral usize) $ \plu -> do
+                            -- rc <- zlib_uncompress pu plu pc (fromIntegral lc)
+                            -- unless (rc == 0) . fail $ "fuck, Zlib don't like me: " ++ show rc
+                            -- print (plu, usize)
+                            -- peek plu >>= guard . (fromIntegral usize ==)
+                            -- B.packCStringLen (pu,fromIntegral usize)
+
 
 -- Run an Iteratee, collect the input.  When it finishes, apply the
 -- continuation and run the result on all input.
-collectI :: (Monad m, Monoid s, Nullable s) => I.Iteratee s m a -> (a -> I.Iteratee s m b) -> I.Iteratee s m b
-collectI it0 k = go mempty it0
+collectI :: (Monad m, Monoid s, I.Nullable s) => I.Iteratee s m a -> (a -> I.Iteratee s m b) -> I.Iteratee s m b
+collectI it0 k0 = go mempty it0
   where 
     go acc it = I.Iteratee $ \od oc -> I.runIter it (onDone od oc) (onCont od oc)
       where
-        onDone od oc x _ = I.enumPure1Chunk acc (k x) >>= \it2 ->
-                           I.runIter it2 od oc
+        onDone od oc x _ = do it2 <- I.enumPure1Chunk acc (k0 x)
+                              I.runIter it2 od oc
         onCont od oc k mErr = I.runIter (I.icont (step k) mErr) od oc
         step k c@(I.EOF _) = go acc (k c)
         step k c@(I.Chunk str)
-          | I.nullC str            = I.liftI (step k)
+          | I.nullC str          = I.liftI (step k)
           | otherwise            = go (acc `mappend` str) (k c)
 
-get_bgzf_header :: Monad m => I.Iteratee ByteString m Word16
+get_bgzf_header :: Monad m => I.Iteratee B.ByteString m Word16
 get_bgzf_header = do 31 <- I.head
                      139 <- I.head
-                     cm <- I.head
+                     _cm <- I.head
                      flg <- I.head
-                     junk <- if flg `testBit` 2 then I.drop 6 else fail "no BGZF"
+                     if flg `testBit` 2 then I.drop 6 else fail "no BGZF"
                      xlen <- endianRead2 LSB 
                      I.joinI $ I.take (fromIntegral xlen) get_bsize
 
@@ -81,7 +93,7 @@ get_bgzf_header = do 31 <- I.head
 #define Z_VERSION_ERROR (-6)
 -}
 
-get_bsize :: Monad m => I.Iteratee ByteString m Word16
+get_bsize :: Monad m => I.Iteratee B.ByteString m Word16
 get_bsize = do i1 <- I.head
                i2 <- I.head
                len <- endianRead2 LSB
@@ -89,48 +101,12 @@ get_bsize = do i1 <- I.head
                   then endianRead2 LSB
                   else I.drop (fromIntegral len) >> get_bsize
 
-get_block :: Monad m => Int -> I.Iteratee ByteString m ByteString
+get_block :: Monad m => Int -> I.Iteratee B.ByteString m B.ByteString
 get_block sz = I.liftI $ \s -> case s of
     I.EOF _ -> I.throwErr $ I.setEOF s 
     I.Chunk c | B.length c < sz -> B.append c `liftM` get_block (sz - B.length c)
               | otherwise       -> I.idone (B.take sz c) (I.Chunk (B.drop sz c))
 
-
-{-
---
---
---
---
--- | decompresses Bgzf or Gzip
--- This checks for the Bgzf header, and if present, decompresses Bgzf
--- chunks.  Else it decompresses GZip.
-decompressBgzf :: L.ByteString -> L.ByteString
-decompressBgzf = go
-  where
-    go s | L.null s = L.empty 
-    go s = case runGet get_bgzf_hdr s of 
-                Nothing -> if isGzip s then decompress s else s
-                Just l  -> case L.splitAt (fromIntegral l + 1) s of 
-                                (u,v) -> decompress u `L.append` go v
-                     
-    get_bgzf_hdr = do id1 <- getWord8
-                      id2 <- getWord8
-                      skip 1
-                      flg <- getWord8
-                      if id1 == 31 && id2 == 139 && flg `testBit` 2 
-                        then do skip 6
-                                xdata <- getWord16le >>= getLazyByteString . fromIntegral
-                                return $ runGet get_bsize xdata
-                        else return Nothing
-
-    get_bsize = isEmpty >>= \e -> if e then return Nothing else do
-                    i1 <- getWord8
-                    i2 <- getWord8
-                    len <- getWord16le
-                    if i1 == 66 && i2 == 67 && len == 2 
-                      then Just <$> getWord16le
-                      else skip (fromIntegral len) >> get_bsize
--}
 
 -- int uncompress (Bytef *dest, uLongf *destLen, const Bytef *source, uLong sourceLen)
 -- foreign import ccall unsafe "zlib.h uncompress" zlib_uncompress
