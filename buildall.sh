0c1d514a6c39efce08fdebde9fd09add86af8699
udo_stenzel <udo_stenzel@93d9a472-daf7-0310-8f21-d7befa554562>
2012-10-04 14:56:50 +0000

    better deployment script
    git-svn-id: http://biofs04/svn/bioinf/biohazard/trunk@3121 93d9a472-daf7-0310-8f21-d7befa554562

--- buildall.sh
+++ buildall.sh
@@ -3,15 +3,24 @@ version=$( awk '/Version:/ { print $2 }' biohazard.cabal )
 echo "Building and installing biohazard-${version}"
 
 cabal clean
-cabal configure --ghc-options=-Wall -O2
+cabal configure --ghc-options=-Wall -O2 --prefix=/home/public/usr64/stow/biohazard-${version}
 cabal build
 if which cabal-src-install ; then
-    cabal-src-install
+    cabal-src-install --prefix=/home/public/usr64/stow/biohazard-${version}
 else
-    cabal install
+    cabal install --prefix=/home/public/usr64/stow/biohazard-${version}
 fi
-# cabal sdist
+cabal sdist
 
-#echo "SCP to bioinf..."
-#scp dist/biohazard-${version}.tar.gz \
-    #bioinf:htdocs/biohazard/
+(
+  cd /home/public/usr64/stow
+  stow -D biohazard-[0-9]*
+  stow biohazard-${version}
+  tar -czf /var/tmp/biohazard-${version}_x86_64.tar.gz biohazard-${version}
+)
+
+echo "SCP to bioinf..."
+scp dist/biohazard-${version}.tar.gz \
+scp /var/tmp/biohazard-${version}_x86_64.tar.gz \
+    bioinf:htdocs/biohazard/
+rm /var/tmp/biohazard-${version}_x86_64.tar.gz 
