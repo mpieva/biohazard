0318fe20c36ae44e2b7f9280f999a2b764ed9efd
Udo Stenzel <udo.stenzel@eva.mpg.de>
2014-01-07 15:36:53 +0100

    added mt-anno, for better or worse

---
+++ mt-tools/Setup.hs
@@ -0,0 +1,26 @@
+import Distribution.Simple
+import Distribution.Simple.Setup ( copyDest, copyVerbosity, fromFlag, installVerbosity )
+import Distribution.PackageDescription      ( PackageDescription(..) )
+import Distribution.Simple.LocalBuildInfo   ( LocalBuildInfo(..), absoluteInstallDirs )
+import Distribution.Verbosity               ( Verbosity )
+import Distribution.Simple.InstallDirs      ( mandir, CopyDest (NoCopyDest) )
+import Distribution.Simple.Utils            ( installOrdinaryFiles  )
+import System.FilePath                      ( splitDirectories, joinPath )
+import System.Exit
+
+main :: IO ()
+main = do
+  defaultMainWithHooks $ simpleUserHooks
+    { postCopy = \ _ flags pkg lbi ->
+         installManpages pkg lbi (fromFlag $ copyVerbosity flags)
+              (fromFlag $ copyDest flags)
+    , postInst = \ _ flags pkg lbi ->
+         installManpages pkg lbi (fromFlag $ installVerbosity flags)
+              NoCopyDest
+    }
+  exitWith ExitSuccess
+
+installManpages :: PackageDescription -> LocalBuildInfo -> Verbosity -> CopyDest -> IO ()
+installManpages pkg lbi verbosity copy =
+    installOrdinaryFiles verbosity (mandir (absoluteInstallDirs pkg lbi copy)) $
+        [ ("man", joinPath mp) | ("man":mp) <- map splitDirectories $ extraSrcFiles pkg ]
