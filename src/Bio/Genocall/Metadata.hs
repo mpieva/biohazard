00f8c62b3540da43a2ce17c09cc043b4d8e8bfcb
Bioinformatics Team <bioinformatics@eva.mpg.de>
2016-08-09 16:09:34 +0200

    support text < 1.2.1

--- src/Bio/Genocall/Metadata.hs
+++ src/Bio/Genocall/Metadata.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, RecordWildCards, FlexibleContexts, BangPatterns #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards, FlexibleContexts, BangPatterns, CPP #-}
 -- | Metadata necessary for a sensible genotyping workflow.
 module Bio.Genocall.Metadata where
 
@@ -10,6 +10,7 @@ import Data.Binary.Get                      ( runGetOrFail )
 import Data.Binary.Put                      ( runPut )
 import Data.ByteString.Lazy                 ( toChunks, readFile )
 import Data.ByteString.Unsafe               ( unsafeUseAsCStringLen )
+import Data.Text.Encoding                   ( decodeUtf8', encodeUtf8 )
 import Foreign.Ptr                          ( castPtr )
 import System.Posix.Files                   ( rename, removeLink )
 import System.Posix.IO
@@ -133,6 +134,16 @@ instance Binary DivTable where
 
 instance ToJSON DivTable where
     toJSON (DivTable a b) = toJSON [toJSON a, toJSON b]
+
+#if !MIN_VERSION_text(1,2,1)
+instance Binary Text where
+    put = put . encodeUtf8
+    get = do
+      bs <- get
+      case decodeUtf8' bs of
+          Left exn -> fail $ show exn
+          Right  a -> return a
+#endif
 
 putObject :: Binary value => M.HashMap Text value -> Put
 putObject m = put (M.size m) >> M.foldrWithKey (\k v a -> put k >> put v >> a) (return ()) m
